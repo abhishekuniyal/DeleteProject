8d0ec9099ace5c723f1570ae293c767f1e5adc49
abhishek <abhishek.uniyal@hotmail.com>
2018-05-15 01:18:14 +0530

    Initial commit

---
+++ src/main/java/com/gotprn/DeleteProjectApplication.java
@@ -0,0 +1,12 @@
+package com.gotprn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteProjectApplication.class, args);
+	}
+}
